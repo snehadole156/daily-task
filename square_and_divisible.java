67bff50eedcd2392091aa86cafbe4963ffa5c58f
snehadole156 <113990426+snehadole156@users.noreply.github.com>
2022-09-20 23:17:18 +0530

    Update and rename switchVowel.java to square_and_divisible.java

---
+++ square_and_divisible.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.math.*;
+class SquareNo 
+{
+    public static void main(String args[])
+    {
+    int no;
+    System.out.println("Enter number:");
+    Scanner sc=new Scanner(System.in);
+    no=sc.nextInt();
+    if(no%5==0)
+    {
+        System.out.println("Square of "+no+ " is "+(no*no));
+    }
+    else
+    {
+        System.out.println("Entered number is not divisible by 5 ");
+   }
+
+    }
+     }
